d29a11415af222d2046b5e0cea6a0fdc190c2924
Laide Endeley <endeleylaide@gmail.com>
2023-09-17 14:20:40 -0700

    Fixed pycodestyle error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,7 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
 class FileStorage:
     """This is the FileStorage Class"""
     __file_path = "file.json"
